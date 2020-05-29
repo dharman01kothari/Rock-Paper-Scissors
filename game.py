b3d78df30bb3ec8adf87449b3142b8fce43ccc1e
dharman01kothari <65413671+dharman01kothari@users.noreply.github.com>
2020-05-28 20:56:32 -0400

    Preliminary Ver

--- game.py
+++ game.py
@@ -1,3 +1,35 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+import random
+
+def check_choice(choice):
+    if choice.upper() not in ["ROCK","PAPER","SCISSORS"]:
+        print("\nYou entered an invalid input - SHAME ON YOU")
+        quit()
+    
+def prompt_choice():
+    return input('Please enter your choice: Rock, Paper or Scissors: ')
+
+if __name__ == "__main__":
+    
+    print('\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+    print("\n  Rock, Paper, Scissors, Shoot! - THE GAME v1.0\n")
+    print('*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*')
+    input('Press enter to continue\n')
+
+#---------USER MOVES------------------
+
+    user_choice = prompt_choice()
+
+    check_choice(user_choice)
+
+    print('\nYou have chosen ', user_choice.upper(),'\n')
+
+#-------------AI MOVES--------------
+
+    ai_choice = random.choice(["ROCK",'PAPER','SCISSORS'])
+
+    print('The AI has chosen ', ai_choice)
+
+#-----------RESULTS-----------------
+
